699091e5c5ef7748628d7563ba512855e8c26462
Kenta Tanaka <tkenta2929@gmail.com>
2022-08-20 15:19:18 -0700

    players change

---
+++ ProgrammingAssignment2/players.py
@@ -0,0 +1,165 @@
+from copy import deepcopy
+import random
+import time
+import pygame
+import math
+import sys
+
+class connect4Player(object):
+	def __init__(self, position, seed=0):
+		self.position = position
+		self.opponent = None
+		self.seed = seed
+		random.seed(seed)
+
+	def play(self, env, move):
+		move = [-1]
+
+class human(connect4Player):
+
+	def play(self, env, move):
+		move[:] = [int(input('Select next move: '))]
+		while True:
+			if int(move[0]) >= 0 and int(move[0]) <= 6 and env.topPosition[int(move[0])] >= 0:
+				break
+			move[:] = [int(input('Index invalid. Select next move: '))]
+
+class human2(connect4Player):
+
+	def play(self, env, move):
+		done = False
+		while(not done):
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+				if event.type == pygame.MOUSEMOTION:
+					pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+					posx = event.pos[0]
+					if self.position == 1:
+						pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+					else: 
+						pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+				pygame.display.update()
+
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					posx = event.pos[0]
+					col = int(math.floor(posx/SQUARESIZE))
+					move[:] = [col]
+					done = True
+
+class randomAI(connect4Player):
+
+	def play(self, env, move):
+		possible = env.topPosition >= 0
+		indices = []
+		for i, p in enumerate(possible):
+			if p: indices.append(i)
+		move[:] = [random.choice(indices)]
+
+class stupidAI(connect4Player):
+
+	def play(self, env, move):
+		possible = env.topPosition >= 0
+		indices = []
+		for i, p in enumerate(possible):
+			if p: indices.append(i)
+		if 3 in indices:
+			move[:] = [3]
+		elif 2 in indices:
+			move[:] = [2]
+		elif 1 in indices:
+			move[:] = [1]
+		elif 5 in indices:
+			move[:] = [5]
+		elif 6 in indices:
+			move[:] = [6]
+		else:
+			move[:] = [0]
+
+class minimaxAI(connect4Player):
+
+	def simulateMove(env, move, turn):  # thus function should just be us placing the pieces on the board 
+		if env.topPosition[move] < 0:
+			possible = env.topPosition >= 0
+			indices = []
+			for i, p in enumerate(possible):
+				if p: indices.append(i)
+			move = random.choice(indices)
+		env.board[env.topPosition[move]][move] = env.turnPlayer.position
+		return env 
+
+	def simulateMove(self, env, move, player):
+		env.board[env.topPosition[move]][move] = player
+		env.topPosition[move] -= 1
+		env.history[0].append(move)
+
+	
+	def eval(self, env):   #this function is our evaluation function that we call in MIN and MAX 
+		# if win return 1
+		# if lose return -1
+		# if tie return 0
+		pass
+		
+
+	def gameOver(env):
+		# if gameisover return true
+		return False
+
+		
+	def MAX(self,env,move, depth): #env is the board 
+		
+		if gameOver(env) or depth == 0:
+			return eval(env.board)
+		possible = env.topPosition >= 0
+		max_v = math.inf
+
+		for move in possible:
+			child = self.simulateMove(deepcopy(env), move, self.position) #simulatemove adding whoever turn it is to environment 
+			max_v = min(max_v, self.MIN(child, depth - 1))
+		return max_v
+
+	
+	def MIN(self, env, move, depth):
+		if gameOver(env) or depth == 0:
+			return eval(env.board)
+		possible = env.topPosition >= 0
+		for move in possible:
+			child = self.simulateMove(deepcopy(env), move, self.position)
+			max_v = min(max_v, self.MAX(child, depth - 1))
+		return max_v 
+
+	
+
+
+class alphaBetaAI(connect4Player):
+
+	def play(self, env, move):
+		pass
+
+
+SQUARESIZE = 100
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ECS-170 - Artificial-Intelligence
+This repository is where I'm going to be showcasing the concepts that I've learned during my time taking ECS 170
